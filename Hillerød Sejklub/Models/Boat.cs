ad7947fc84fb0585babbdf8cf7865b03bdbd4ea7
HelinSaraBaran <sara501d@edu.zealand.dk>
2025-04-29 13:44:47 +0200

    Tilføjet klasser

---
+++ Hillerød Sejklub/Models/Boat.cs
@@ -0,0 +1,33 @@
+﻿public class Boat
+{
+    public int BoatId { get; set; }
+    public string Name { get; set; }
+    public string Model { get; set; }
+    public string SailNumber { get; set; }
+    public string EngineInfo { get; set; }
+    public int BuildYear { get; set; }
+    public bool IsAvailable { get; set; }
+
+    public Boat(int BoatID, string name, string model, string sailNumber, string engineInfo, int builderYear, bool isAvailable)
+    {
+        Name = name;
+        Model = model;
+        SailNumber = sailNumber;
+        EngineInfo = engineInfo;
+        BuildYear = builderYear;
+        IsAvailable = isAvailable;
+
+    }
+    public override string ToString()
+    {
+        return $"{Name},{Model},{SailNumber},{EngineInfo}, {IsAvailable}";
+    }
+
+    public List<Boat> Boattype = new List<Boat>();
+    public void AddBoat(Boat boat)
+    {
+        Boattype.Add(boat);
+    }
+
+
+}
---
+++ Hillerød Sejklub/Models/Booking.cs
@@ -0,0 +1,56 @@
+﻿namespace Hillerød_Sejklub.Models
+{
+    public class boatBooking
+    {
+        public int BookingID { get; set; }
+        public int BådID { get; set; }
+        public int MedlemsID { get; set; }
+        public DateTime SejlTid { get; set; }
+        public string Mødested { get; set; }
+
+
+
+        List<boatBooking> bookings = new List<boatBooking>();
+
+        public int GenerateBookingID()
+        {
+            return BookingID++;
+        }
+
+        public void BookBåd(int bådID, int medlemsID, DateTime sejlTid, string mødested)
+        {
+            // Generer et unikt ID for booking (f.eks. øjebliksbillede eller tæl op)
+            int nyBookingID = GenerateBookingID();
+
+            // Opret en ny booking
+            boatBooking nyBooking = new boatBooking
+            {
+                BookingID = nyBookingID,
+                BådID = bådID,
+                MedlemsID = medlemsID,
+                SejlTid = sejlTid,
+                Mødested = mødested
+            };
+
+
+
+            // Gem bookingen
+            bookings.Add(nyBooking);
+
+            Console.WriteLine("Båd er blevet booket.");
+        }
+
+
+        public bool ErBådTilgængelig(int bådID, DateTime ønsketTid)
+        {
+            foreach (var booking in bookings)
+            {
+                if (booking.BådID == bådID && booking.SejlTid == ønsketTid)
+                {
+                    return false; // Båden er allerede booket
+                }
+            }
+            return true; // Båden er tilgængelig
+        }
+    }
+}
