241c1ed9887a1a30e0c02fbb8ff1e5e08ab4819f
HelinSaraBaran <sara501d@edu.zealand.dk>
2025-04-29 12:04:25 +0200

    repairrequest.cs

---
+++ Hillerød Sejklub/Pages/RepairRequest.cs
@@ -0,0 +1,22 @@
+﻿namespace Hillerød_Sejklub.Pages
+{
+
+    // Class representing repair request for a boat  
+    public class RepairRequest
+    {
+        // Public properties for a repair request  
+        public int Id { get; set; }
+        public string BoatName { get; set; } // Fix type mismatch: Change type from int to string  
+        public string ProblemDescription { get; set; }
+        public bool IsFixed { get; set; }
+
+        // Constructor for repair request  
+        public RepairRequest(int id, string boatName, string problemDescription, bool isFixed)
+        {
+            Id = id;
+            BoatName = boatName;
+            ProblemDescription = problemDescription;
+            IsFixed = isFixed;
+        }
+    }
+}
